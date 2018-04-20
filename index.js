80b4f119402c265de087dbd28bbeeac55d32ad88
smartworld1004 <smartworld1004@outlook.com>
2018-04-20 12:13:56 +0300

    2nd commit

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-'use strict';
-
-/**
- * Adds commas to a number
- * @param {number} number
- * @param {string} locale
- * @return {string}
- */
-module.exports = function(number, locale) {
-    return number.toLocaleString(locale);
+module.exports = {
+  escape: function(html) {
+    return String(html)
+      .replace(/&/g, '&amp;')
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&#39;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;');
+  },
+  
+  unescape: function(html) {
+    return String(html)
+      .replace(/&amp;/g, '&')
+      .replace(/&quot;/g, '"')
+      .replace(/&#39;/g, ''')
+      .replace(/&lt;/g, '<')
+      .replace(/&gt;/g, '>');
+  }
 };
